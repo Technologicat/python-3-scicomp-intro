8050b9795493493b6d8c5cfb331b1a528f016ab0
Juha Jeronen <juha.jeronen@jyu.fi>
2017-10-20 15:32:03 +0300

    add a purely functional uniqify2()

--- examples/beyond_python/uniqify.rkt
+++ examples/beyond_python/uniqify.rkt
@@ -1,6 +1,12 @@
 #!/usr/bin/env racket
 #lang sweet-exp racket
 
+;; If we use filter, we must store the seen status of each item as a side-effect,
+;; because filter only feeds its predicate one item at a time, and expects only
+;; a boolean as output (so there is no easy way to pass state in a functional style).
+;;
+;; Hence, this is somewhat imperative, but the code is very short.
+;;
 define uniqify(L)
   define seen null
   define unique?(x)
@@ -8,6 +14,24 @@ define uniqify(L)
          [else #f]
   filter unique? L
 
+;; Functional style:
+;;   - state is passed as parameters
+;;   - tail recursion
+;; We filter manually.
+;;
+define uniqify2(L)
+  let keep-if-unique ([input L]
+                      [seen null]
+                      [output null])
+    cond [(empty? input) reverse(output)]  ; done
+         [else
+            define(x car(input))
+            define(the-rest cdr(input))
+            cond(
+              [(not member(x seen)) keep-if-unique(the-rest (cons x seen) (cons x output))]
+              [else keep-if-unique(the-rest seen output)])]
+
 module+ main
   define L '(2 1 2 1 3 3 3 4)
   uniqify L
+  uniqify2 L
