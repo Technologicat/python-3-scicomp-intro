4860f81c57ce6253b1fc8d81b4d4f548be32fa92
Juha Jeronen <juha.jeronen@jyu.fi>
2018-02-13 16:58:32 +0200

    uniqify_python.py: add one-liner version; clean up whitespace; use genexpr instead of filter() (more idiomatic in Python 3)

--- examples/beyond_python/uniqify_python.py
+++ examples/beyond_python/uniqify_python.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+def uniqify_oneliner(L):  # CAUTION: generally considered unreadable in the Python community
+    return (lambda seen: [seen.add(x) or x for x in L if x not in seen])(set())
+
 def uniqify_modern(L):
     seen = set()
-    return ( seen.add(x) or x for x in L if x not in seen )
+    return (seen.add(x) or x for x in L if x not in seen)
 
 def uniqify_classic(L):
     seen = set()
@@ -13,9 +16,10 @@ def uniqify_classic(L):
             return True
         else:
             return False
-    return filter(isunique, L)
+    return (x for x in L if isunique(x))  # equivalent with filter(isunique, L)
 
 L = (2, 1, 2, 1, 3, 3, 3, 4)
+print(tuple(uniqify_oneliner(L)))
 print(tuple(uniqify_modern(L)))
 print(tuple(uniqify_classic(L)))
 
