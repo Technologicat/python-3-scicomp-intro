d7755ed8f84ad62cd7cfaf89c3fba6c8a9a2acdb
Juha Jeronen <juha.jeronen@jyu.fi>
2018-02-13 16:42:19 +0200

    update comments

--- examples/beyond_python/sieve.rkt
+++ examples/beyond_python/sieve.rkt
@@ -1,28 +1,58 @@
 #lang sweet-exp racket
 
-;;; Sieve of Eratosthenes using Racket's generators, packaged into infinite streams,
-;;; using streams from Racket's standard library.
+;;; Compute prime numbers. Sieve of Eratosthenes using Racket's generators.
 ;;;
-;;; A generator can be packaged into a sequence (using "in-producer"), which can be
-;;; further packaged into a stream, which then supports "stream-first" and "stream-rest".
+;;; For the best implementation, see sieve3.rkt.
 ;;;
-;;; See the documentation:
-;;;   https://docs.racket-lang.org/reference/Generators.html
-;;;   https://docs.racket-lang.org/reference/sequences.html
-;;;   https://docs.racket-lang.org/reference/streams.html
+;;; For an implementation using custom infinite lists, see sieve2.rkt.
 ;;;
-;;; For a version using plain infinite streams, see SICP 2nd ed., sec. 3.5.1.
+;;; For the original inspiration, see SICP 2nd ed., sec. 3.5.1.
 ;;;   https://mitpress.mit.edu/sicp/full-text/book/book-Z-H-24.html#%_sec_3.5.1
+
+;;; This is a direct translation of the Python example into Racket, and as the length of the program
+;;; shows, this particular packaging of the idea is definitely more pythonic than rackety! In Python,
+;;; a generator (once invoked) is just an iterable, so the use site sees it as a lazy list.
+;;;
+;;; Not so in Racket, which makes a distinction between generators, sequences and streams:
+;;;
+;;; - A generator, once invoked, is basically a procedure; calling it returns the next value.
+;;;
+;;;   This works just like in Python: recall that invoking a Python generator returns an iterator,
+;;;   and you extract values from the generator by calling next() on this iterator.
+;;;
+;;;   Racket however makes the distinction that strictly speaking, it makes no sense to iterate
+;;;   over /a procedure/. To iterate over the values the generator yields, we use "in-producer"
+;;;   to tell Racket to package the generator into a sequence first.
+;;;
+;;; - A sequence is an abstract data type for an ordered collection of values; one can "for" over it.
+;;;
+;;; - A stream is a subtype of sequence that understands the concepts of "first" and "rest",
+;;;   needed in the classic list processing paradigm (process first element, loop over the rest).
+
+;;; Relevant parts of Racket documentation.
+;;;
+;;;   Gentle introduction to sequences:
+;;;     https://docs.racket-lang.org/guide/for.html#%28part._sequences%29
+;;;
+;;;   API reference:
+;;;     https://docs.racket-lang.org/reference/Generators.html
+;;;     https://docs.racket-lang.org/reference/sequences.html
+;;;     https://docs.racket-lang.org/reference/streams.html
 
 require racket/generator
 
 define divisible(n k)
   {(remainder n k) = 0}  ; n mod k
 
+;; Just like in the Python example, we define two generators that produce streams.
+
+;; Take a stream, and return a stream with multiples of m filtered out.
 define remove-multiples-of(m stream)
-  sequence->stream
-    in-producer
-      generator ()
+  ;; Three levels of wrappers? The complexity here hints that we are likely missing something;
+  ;; surely there must be a more rackety way of doing this! (See sieve3.rkt for the solution.)
+  sequence->stream   ; Package as a stream the...
+    in-producer      ; ...sequence that encapsulates the...
+      generator ()   ; ...generator, which is defined as:
         let loop ([s stream])
           define k stream-first(s)
           cond
--- examples/beyond_python/sieve2.rkt
+++ examples/beyond_python/sieve2.rkt
@@ -2,6 +2,8 @@
 
 ;;; Sieve of Eratosthenes using infinite streams, custom implementation (see stream.rkt).
 ;;;
+;;; This is a classical Lisp approach to the problem.
+;;;
 ;;; See SICP 2nd ed., sec. 3.5.1.
 ;;;   https://mitpress.mit.edu/sicp/full-text/book/book-Z-H-24.html#%_sec_3.5.1
 
@@ -14,17 +16,18 @@ letrec
   \\
     ones
       scons 1 ones
-    naturals-from
-      λ (n)
+    naturals-from  ; needs to obey the custom stream interface, hence Racket's "in-naturals" won't do.
+      λ (n)  ; letrec has no "implicit lambda" for functions, unlike define, so do it manually.
         scons n (sadd ones (naturals-from n))
     sieve
       λ (s)
+        define m (scar s)
         scons
-          scar s  ; invariant: the first element of s is prime
-          sieve
+          m       ; invariant: the first element of s is prime (so output it)
+          sieve   ; in the tail, we store the procedure to compute the rest of the stream
             sfilter
               λ (x)
-                not (divisible x (scar s))
+                not (divisible x m)
               scdr s
     primes
       sieve (naturals-from 2)
@@ -32,4 +35,6 @@ letrec
   define lst (s->list primes #:nterms 100)
   displayln lst
   ;
-  displayln (sref primes 200)  ; find the 200th prime
+  ;; As Abelson and Sussman point out in SICP, the beauty of the stream abstraction
+  ;; is that we can find the nth prime by just indexing into the stream:
+  displayln (sref primes 200)
