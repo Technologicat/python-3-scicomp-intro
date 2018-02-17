3e7e5337cffd7dce49068de38c48ce5884e025c4
Juha Jeronen <juha.jeronen@jyu.fi>
2018-02-18 00:45:40 +0200

    amb.rkt: de-confuse the example a bit

--- examples/beyond_python/amb.rkt
+++ examples/beyond_python/amb.rkt
@@ -14,7 +14,7 @@ define orig-fail fail
 
 define amb(. params)
   displayln
-    format "amb, params ~a" params
+    format "  amb called, params ~a" params
   cond
     empty?(params)
       set! fail orig-fail
@@ -23,36 +23,44 @@ define amb(. params)
       let/cc cc
         set! fail
           λ ()
-            cc (apply amb (cdr params))
-        car params
+            cc (apply amb (map force (cdr params)))  ; forcing allows us to use "delay foo" as the cdr
+        force (car params)  ; just in case (force on a value is harmless)
 
 define assert(pred expr)
   displayln
-    format "assert ~a ~a" pred expr
+    format "  assert ~a ~a" pred expr
   cond
     eq?(expr 'no-solution)
-      displayln "  no solution"
+      displayln "    no solution"
       #f
     pred(expr)
-      displayln "  ok"
+      displayln "    ok"
       expr
     else
-      displayln "  fail"
+      displayln "    fail"
       fail()
 
 module+ main
-  ;; We can't amb in a define, because the define would then be part of the continuation,
-  ;; trying to re-define the same identifier when we fail().
-  ;;
-  ;; Instead, define first, then set!; to make fail() re-run only the set!.
-  define foo 'not-initialized
-  set! foo
-    assert even? (amb 1 -1 2 3 5 7 8)
-  foo
-  fail()
-  foo
-  fail()
-  foo
-  ;;
-  ;; This implementation is too simple to support the pythagorean triples example;
-  ;; since there is no stack, multiple simultaneous "amb"s do not work. See choice.rkt.
+  define main()
+    displayln "Hi! Demonstrating amb():"
+    ;; We can't amb in a define, because the define would then be part of the continuation,
+    ;; trying to re-define the same identifier when we fail().
+    ;;
+    ;; Instead, define first, then set!; to make fail() re-run only the set!.
+    define foo 'not-initialized
+    set! foo
+      assert even? (amb 1 -1 2 3 5 7 8)
+    displayln "Hi again!"
+    cond
+      foo
+        displayln
+          format
+            "Found solution: ~a"
+            foo
+      else
+        displayln "No more solutions found."
+    fail()  ; jumps back to the amb, unless no more solutions
+    ;;
+    ;; This implementation is too simple to support the pythagorean triples example;
+    ;; since there is no stack, multiple simultaneous "amb"s do not work. See choice.rkt.
+  main()
