726139a7dc7d9ec4c3e92c48ed31bbe4c47fcb2c
Juha Jeronen <juha.jeronen@jyu.fi>
2018-05-01 00:16:28 +0300

    improve documentation, naming, add a let block

--- examples/let.py
+++ examples/let.py
@@ -15,16 +15,28 @@ Created on Mon Apr 30 13:51:47 2018
 """
 
 class env:
-    """Context manager aware bunch. Also works as a bare bunch."""
+    """Context manager aware bunch. Also works as a bare bunch.
+
+    Usage:
+        # with context manager:
+        with env(x = 0) as myenv:
+            print(myenv.x)
+        # DANGER: myenv still exists due to Python's scoping rules.
+
+        # bare bunch:
+        myenv2 = env(s="hello", orange="fruit", answer=42)
+        print(myenv2.s)
+        print(myenv2)
+"""
     def __init__(self, **bindings):
         self._env = {}
         for name,value in bindings.items():
             self._env[name] = value
 
     def __setattr__(self, name, value):
-        if name == "_env":
-            return super().__setattr__(name, value)  # allow creating _env in self
-        self._env[name] = value  # otherwise make everything live inside _env
+        if name == "_env":  # hook to allow creating _env directly in self
+            return super().__setattr__(name, value)
+        self._env[name] = value  # make all other attrs else live inside _env
 
     def __getattr__(self, name):
         env = self._env   # __getattr__ not called if direct attr lookup succeeds, no need for hook.
@@ -37,22 +49,25 @@ class env:
         return self
 
     def __exit__(self, et, ev, tb):
+        # we could nuke the *contents*, but it's simpler and more predictable not to.
         pass
 
     def __str__(self):  # just for pretty-printing
         bindings = ["{}: {}".format(name,value) for name,value in self._env.items()]
         return "<env: <{:s}>>".format(", ".join(bindings))
 
-def with_let(body, **bindings):
+def letexpr(body, **bindings):
     """let expression, for use with lambdas.
 
     Example: a list uniqifier:
 
-        f = lambda lst: with_let(seen=set(),
-                                 body=lambda env: [env.seen.add(x) or x for x in lst if x not in env.seen])
+        f = lambda lst: letexpr(seen=set(),
+                                body=lambda env: [env.seen.add(x) or x for x in lst if x not in env.seen])
 
     Parameters:
         `body`: one-argument function to run, taking an `env` instance that contains the "let" bindings.
+                (Take advantage of free variables and lexical scoping to pass in other stuff if needed.)
+
         Everything else: "let" bindings; each argument name is bound to its value.
 
     Returns:
@@ -61,15 +76,29 @@ def with_let(body, **bindings):
     return body(env(**bindings))
 
 # decorator factory: almost as fun as macros?
-def let(**bindings):                     # decorator factory
+def let_over_def(**bindings):            # decorator factory
     """let decorator, for use with named functions.
+
+    Named after "let over lambda", since this version of "let" can only
+    sit above a "def".
+
+    Usage is similar to the Lisp idiom: this gives a local environment
+    that can be used to stash data to be preserved between calls.
 
     Usage:
 
-    @let(y = 23, z = 42)
+    @let_over_def(y = 23, z = 42)
     def foo(x, env=None):  # env is filled in by the decorator
         print(x, env.y, env.z)
     foo(17)
+
+    @let_over_def(count = 0)
+    def counter(env=None):
+        env.count += 1
+        return env.count
+    print(counter())
+    print(counter())
+    print(counter())
 
     The named argument `env` is an env instance that contains the let bindings;
     all other args and kwargs are passed through.
@@ -84,6 +113,54 @@ def let(**bindings):                     # decorator factory
             kwargs_with_env["env"] = env_instance
             return body(*args, **kwargs_with_env)
         return decorated
+    return deco
+
+def immediate(thunk):
+    """Decorator: run immediately, overwrite function by its return value.
+
+    Can be used to make lispy not-quite-functions where the def just delimits
+    a block of code that runs immediately (like call-with-something in Lisp).
+    Convenient e.g. for escaping nested loops (by using return).
+
+    The function must be callable with zero arguments.
+
+    Usage:
+
+    @immediate
+    def result():  # this block of code runs immediately
+        return "hello"
+    print(result)  # "hello"
+
+    # can use a dummy name if the return value is of no interest:
+    @immediate
+    def _():
+        ... # code with cheeky side effects goes here
+"""
+    return thunk()
+
+def let(**bindings):
+    """let block, for use with a def.
+
+    This is a decorator chain, first applying @let_over_def, then @immediate.
+
+    In effect, this makes the def the body of the let, and runs it immediately.
+
+    Usage:
+
+    @let(x=17, y=23)
+    def result(env=None):
+        print(env.x, env.y)
+        return env.x + env.y
+    print(result)  # 40
+
+    # can use a dummy name if the return value is of no interest:
+    @let(s="hello")
+    def _(env=None):
+        print(env.s)
+"""
+    let_over_def_deco = let_over_def(**bindings)
+    def deco(body):
+        return immediate(let_over_def_deco(body))
     return deco
 
 
@@ -115,8 +192,8 @@ def i(_myenv = env(x = 0)):
 
 # The decorator factory also gives us true "let over lambda" behavior:
 #
-@let(x = 0)
-def j(env=None):
+@let_over_def(x = 0)
+def j(env):
     env.x += 1
     return env.x
 
@@ -138,9 +215,9 @@ def uniqify_test():
         with env(seen = set()) as myenv:
             return [myenv.seen.add(x) or x for x in lst if x not in myenv.seen]
 
-    # using the above with_let allows us to write this using only lambdas:
-    f4 = lambda lst: with_let(seen=set(),
-                              body=lambda env: [env.seen.add(x) or x for x in lst if x not in env.seen])
+    # using the above letexpr:
+    f4 = lambda lst: letexpr(seen=set(),
+                             body=lambda env: [env.seen.add(x) or x for x in lst if x not in env.seen])
 
     # testing:
     #
@@ -183,20 +260,21 @@ if __name__ == '__main__':
 
     ################################
 
-    @let(y = 23)
-    def foo(x, env=None):  # the named argument env contains the let bindings
+    @let_over_def(y = 23)
+    def foo(x, env):  # the named argument env contains the let bindings
         print(x, env.y)
     foo(17)
 
     ################################
 
-    # lexical scoping (kinda)
-    @let(x = 5)
-    def bar(env=None):
+    # Lexical scoping - actually, just by borrowing Python's:
+    #
+    @let_over_def(x = 5)
+    def bar(env):
         print("in bar x is", env.x)
 
-        @let(x = 42)
-        def baz(env=None):
+        @let_over_def(x = 42)
+        def baz(env):  # this env shadows the outer env
             print("in baz x is", env.x)
         baz()
 
@@ -205,19 +283,30 @@ if __name__ == '__main__':
 
     ################################
 
-    # To eliminate the explicit calls:
-
-    def immediate(thunk):
-        return thunk()
-
+    # To eliminate the explicit calls, @immediate from lecture 11, slide 33:
+    #
     @immediate
-    @let(x = 5)
-    def _(env=None):
+    @let_over_def(x = 5)
+    def _(env):  # this is now the let block
         print("outer x is", env.x)
 
         @immediate
+        @let_over_def(x = 42)
+        def _(env):
+            print("inner x is", env.x)
+
+        print("outer x is still", env.x)
+
+    ################################
+
+    # With a combined decorator:
+    #
+    @let(x = 5)
+    def _(env):  # the body of the let block
+        print("outer x is", env.x)
+
         @let(x = 42)
-        def _(env=None):
+        def _(env):
             print("inner x is", env.x)
 
         print("outer x is still", env.x)
