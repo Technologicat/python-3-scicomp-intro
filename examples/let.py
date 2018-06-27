52cc9112df3cf7bb0079bd299fbf07c867dce729
Juha Jeronen <juha.jeronen@jyu.fi>
2018-06-28 02:30:38 +0300

    let.py: the great rename

--- examples/let.py
+++ examples/let.py
@@ -112,6 +112,14 @@ Inspiration:
     https://stackoverflow.com/questions/12219465/is-there-a-python-equivalent-of-the-haskell-let
     http://sigusr2.net/more-about-let-in-python.html
 
+TODO:
+  - make a separate project on GitHub
+  - clean up the separate project
+    - figure out a consistent philosophy
+    - remove any unnecessary stuff not in line with that
+  - package as a library, with setuptools
+  - upload to PyPI
+
 Created on Mon Apr 30 13:51:47 2018
 
 @author: Juha Jeronen <juha.jeronen@tut.fi>
@@ -208,27 +216,27 @@ class env:
         return value  # for convenience
 
 
-def letexpr(body, **bindings):
-    """let expression, for use with lambdas.
+def let(body, **bindings):
+    """let expression.
 
     Examples:
 
         # order-preserving list uniqifier
-        f = lambda lst: letexpr(seen=set(),
-                                body=lambda env: [env.seen.add(x) or x for x in lst if x not in env.seen])
+        f = lambda lst: let(seen=set(),
+                            body=lambda env: [env.seen.add(x) or x for x in lst if x not in env.seen])
 
         # a lambda that uses a locally defined lambda as a helper
-        g = letexpr(square=lambda x: x**2,
-                    body=lambda env: lambda x: 42 * env.square(x))
+        g = let(square=lambda x: x**2,
+                body=lambda env: lambda x: 42 * env.square(x))
         print(g(10))
 
     As Lisp programmers know, the second example is subtly different from:
 
-        g = lambda x: letexpr(square=lambda y: y**2,
-                              body=lambda env: 42 * env.square(x))
+        g = lambda x: let(square=lambda y: y**2,
+                          body=lambda env: 42 * env.square(x))
 
-    In the original version, the letexpr runs just once, when g is defined,
-    whereas in this one, it re-runs whenever g is called.
+    In the original version, the let expression runs just once, when g is
+    defined, whereas in this one, it re-runs whenever g is called.
 
     Parameters:
         `body`: one-argument function to run, taking an `env` instance that
@@ -248,26 +256,44 @@ def letexpr(body, **bindings):
     return body(env(**bindings))
 
 
-def letrecexpr(body, **bindings):
-    """letrec expression, for use with lambdas.
+def letrec(body, **bindings):
+    """letrec expression.
 
     The bindings have mutually recursive name resolution, like in Scheme.
 
-    In letrecexpr, also the bindings must be wrapped with a "lambda env:",
+    In letrec, also the bindings must be wrapped with a "lambda env:",
     to delay their evaluation until the environment instance has been created
-    (and can thus be supplied to them).
+    (and can thus be supplied to them, just before the body starts).
 
-    In the actual definitions, the names can be used just like any let-defined
-    names, as env.*.
+    The values for the bindings, inside the  lambda env: ...  wrapper, must be
+    either expressions that do not use env (just like in a regular let),
+    or lambdas (that may use env).
+
+    Caveats:
+
+        - When the arguments to the letrec are evaluated, the environment
+          has not yet been created. The  lambda env: ...  works around this.
+
+        - Initialization of the bindings occurs in an arbitrary order,
+          because of the kwargs mechanism and storage as a dictionary.
+          Hence the following does **not** work:
+
+             x = letrec(a=lambda env: 1
+                        b=lambda env: env.a + 1,
+                        body=lambda env: env.b)
+
+          because trying to reference env.a in the RHS of "b" may either get the
+          lambda env: ..., or the value 1, depending on whether the binding "a"
+          has been initialized at that point or not.
 
     Example:
 
-        t = letrecexpr(evenp=lambda env: lambda x: (x == 0) or env.oddp(x - 1),
-                       oddp=lambda env: lambda x: (x != 0) and env.evenp(x - 1),
-                       body=lambda env: env.evenp(42))
+        t = letrec(evenp=lambda env: lambda x: (x == 0) or env.oddp(x - 1),
+                   oddp=lambda env: lambda x: (x != 0) and env.evenp(x - 1),
+                   body=lambda env: env.evenp(42))
 
     Parameters:
-        `body`: like in letexpr()
+        `body`: like in let()
 
         Everything else: "letrec" bindings, as one-argument functions.
         The argument is the environment.
@@ -293,29 +319,30 @@ def letrecexpr(body, **bindings):
 
 
 # decorator factory: almost as fun as macros?
-def let_over_def(**bindings):            # decorator factory
-    """let decorator, for use with named functions.
+def dlet(**bindings):            # decorator factory
+    """let decorator.
 
-    Named after "let over lambda", since this version of "let" can only
-    sit above a "def".
+    Allows "let over def" for named functions.
 
-    Usage is similar to the Lisp idiom: this gives a local environment
-    that can be used to stash data to be preserved between calls.
+    Usage is similar to the Lisp idiom "let over lambda": this gives a local
+    environment that can be used to stash data to be preserved between calls.
+
+    The "d" in the name stands for "decorator" or "def", your choice.
 
     Usage:
 
-    @let_over_def(y = 23, z = 42)
-    def foo(x, env=None):  # env is filled in by the decorator
-        print(x, env.y, env.z)
-    foo(17)
+        @dlet(y = 23, z = 42)
+        def foo(x, env=None):  # env is filled in by the decorator
+            print(x, env.y, env.z)
+        foo(17)
 
-    @let_over_def(count = 0)
-    def counter(env=None):
-        env.count += 1
-        return env.count
-    print(counter())
-    print(counter())
-    print(counter())
+        @dlet(count = 0)
+        def counter(env=None):
+            env.count += 1
+            return env.count
+        print(counter())
+        print(counter())
+        print(counter())
 
     The named argument `env` is an env instance that contains the let bindings;
     all other args and kwargs are passed through.
@@ -333,10 +360,10 @@ def let_over_def(**bindings):            # decorator factory
     return deco
 
 
-def letrec_over_def(**bindings):
-    """letrec decorator, for use with named functions.
+def dletrec(**bindings):
+    """letrec decorator.
 
-    Like let_over_def, but for letrec.
+    Like dlet, but for letrec.
     """
     def deco(body):
         # evaluate env when the function def runs!
@@ -361,7 +388,7 @@ def immediate(thunk):
     a block of code that runs immediately (like call-with-something in Lisp).
     Convenient e.g. for escaping nested loops (by using return).
 
-    The function must be callable with zero arguments.
+    The function will be called with zero arguments.
 
     Usage:
 
@@ -378,29 +405,40 @@ def immediate(thunk):
     return thunk()
 
 
-def let(**bindings):
-    """let block, for use with a def.
+def blet(**bindings):
+    """let block.
 
-    This is a decorator chain, first applying @let_over_def, then @immediate.
+    This is a decorator chain, first applying @dlet, then @immediate.
 
-    In effect, this makes the def the body of the let, and runs it immediately.
+    In effect, this makes the def the body of the let, runs it immediately,
+    and overwrites the def'd name with the result.
 
     Usage:
 
-    @let(x=17, y=23)
+    @blet(x=17, y=23)
     def result(env=None):
         print(env.x, env.y)
         return env.x + env.y
     print(result)  # 40
 
     # can use a dummy name if the return value is of no interest:
-    @let(s="hello")
+    @blet(s="hello")
     def _(env=None):
         print(env.s)
     """
-    let_over_def_deco = let_over_def(**bindings)
+    dlet_deco = dlet(**bindings)
     def deco(body):
-        return immediate(let_over_def_deco(body))
+        return immediate(dlet_deco(body))
+    return deco
+
+
+def bletrec(**bindings):
+    """letrec block.
+
+    Like blet, but for letrec."""
+    dletrec_deco = dletrec(**bindings)
+    def deco(body):
+        return immediate(dletrec_deco(body))
     return deco
 
 
@@ -432,7 +470,7 @@ def i(_myenv = env(x = 0)):
 
 # The decorator factory also gives us true "let over lambda" behavior:
 #
-@let_over_def(x = 0)
+@dlet(x = 0)
 def j(env):
     env.x += 1
     return env.x
@@ -455,9 +493,9 @@ def uniqify_test():
         with env(seen = set()) as myenv:
             return [myenv.seen.add(x) or x for x in lst if x not in myenv.seen]
 
-    # using the above letexpr:
-    f4 = lambda lst: letexpr(seen=set(),
-                             body=lambda env: [env.seen.add(x) or x for x in lst if x not in env.seen])
+    # using the above let:
+    f4 = lambda lst: let(seen=set(),
+                         body=lambda env: [env.seen.add(x) or x for x in lst if x not in env.seen])
 
     # testing:
     #
@@ -500,7 +538,7 @@ if __name__ == '__main__':
 
     ################################
 
-    @let_over_def(y = 23)
+    @dlet(y = 23)
     def foo(x, env):  # the named argument env contains the let bindings
         print(x, env.y)
     foo(17)
@@ -509,11 +547,11 @@ if __name__ == '__main__':
 
     # Lexical scoping - actually, just by borrowing Python's:
     #
-    @let_over_def(x = 5)
+    @dlet(x = 5)
     def bar(env):
         print("in bar x is", env.x)
 
-        @let_over_def(x = 42)
+        @dlet(x = 42)
         def baz(env):  # this env shadows the outer env
             print("in baz x is", env.x)
         baz()
@@ -526,12 +564,12 @@ if __name__ == '__main__':
     # To eliminate the explicit calls, @immediate from lecture 11, slide 33:
     #
     @immediate
-    @let_over_def(x = 5)
+    @dlet(x = 5)
     def _(env):  # this is now the let block
         print("outer x is", env.x)
 
         @immediate
-        @let_over_def(x = 42)
+        @dlet(x = 42)
         def _(env):
             print("inner x is", env.x)
 
@@ -541,11 +579,11 @@ if __name__ == '__main__':
 
     # With a combined decorator:
     #
-    @let(x = 5)
+    @blet(x = 5)
     def _(env):  # the body of the let block
         print("outer x is", env.x)
 
-        @let(x = 42)
+        @blet(x = 42)
         def _(env):
             print("inner x is", env.x)
 
@@ -623,21 +661,33 @@ if __name__ == '__main__':
 
     # Let over lambda, expression version.
     # The inner lambda is the definition of the function f.
-    f = letexpr(x = 0,
-                body = lambda env: lambda: begin(env.set("x", env.x + 1),
-                                                 env.x))
+    f = let(x = 0,
+            body = lambda env: lambda: begin(env.set("x", env.x + 1),
+                                             env.x))
     print(f())
     print(f())
     print(f())
 
+    # expression
+    #
     # https://docs.racket-lang.org/reference/let.html
-    t = letrecexpr(evenp=lambda env: lambda x: (x == 0) or env.oddp(x - 1),
-                   oddp=lambda env: lambda x: (x != 0) and env.evenp(x - 1),
-                   body=lambda env: env.evenp(42))
+    t = letrec(evenp=lambda env: lambda x: (x == 0) or env.oddp(x - 1),
+               oddp=lambda env: lambda x: (x != 0) and env.evenp(x - 1),
+               body=lambda env: env.evenp(42))
     print(t)
 
-    @letrec_over_def(evenp=lambda env: lambda x: (x == 0) or env.oddp(x - 1),
-                     oddp=lambda env: lambda x: (x != 0) and env.evenp(x - 1))
+    # decorator
+    #
+    @dletrec(evenp=lambda env: lambda x: (x == 0) or env.oddp(x - 1),
+             oddp=lambda env: lambda x: (x != 0) and env.evenp(x - 1))
     def is_even(x, *, env):  # make env passable by name only
         return env.evenp(x)
     print(is_even(23))
+
+    # decorator with immediate
+    #
+    @bletrec(evenp=lambda env: lambda x: (x == 0) or env.oddp(x - 1),
+             oddp=lambda env: lambda x: (x != 0) and env.evenp(x - 1))
+    def result(env):
+        return env.evenp(23)
+    print(result)
