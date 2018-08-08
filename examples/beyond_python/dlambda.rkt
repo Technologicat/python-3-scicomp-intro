14a8a7dade93c26c63d4713dc4632ed039ba17dd
Juha Jeronen <juha.jeronen@jyu.fi>
2018-08-08 23:41:35 +0300

    oops, tests should go in module main; also add a provide

--- examples/beyond_python/dlambda.rkt
+++ examples/beyond_python/dlambda.rkt
@@ -3,6 +3,8 @@
 require syntax/parse/define
 require "abbrev.rkt"
 
+provide dlambda λ/dispatch define-dispatcher
+
 ;; Dispatching λ, for easily making closures with multiple methods.
 ;;
 ;; Inspired by Doug Hoyte's Let Over Lambda: 50 Years of Lisp.
@@ -52,44 +54,56 @@ define-syntax-parser define-dispatcher
 
 abbrev dlambda λ/dispatch
 
-;; We want to achieve something like this:
-define f1
-  λ (#:m [name '()] . args)
-    cond
-      {name eq? 'hello}
-        displayln "hello"
-      {name eq? 'hi}
+module+ main
+  ;; We want to achieve something like this:
+  define f1
+    λ (#:m [name '()] . args)
+      cond
+        {name eq? 'hello}
+          displayln "hello"
+        {name eq? 'hi}
+          displayln "hi"
+        {name eq? 'disp}
+          displayln (car args)
+        else
+          #f
+  ;
+  ;; We can easily generate something like this:
+  define f2
+    λ (#:m [name '()] . args)
+      define hello
+        λ ()
+          displayln "hello"
+      define hi
+        λ ()
+          displayln "hi"
+      define disp
+        λ (s)
+          displayln s
+      cond
+        {name eq? 'hello}
+          (apply hello args)
+        {name eq? 'hi}
+          (apply hi args)
+        {name eq? 'disp}
+          (apply disp args)
+        else
+          #f
+  ;
+  ;; Automated macro version:
+  define f3
+    λ/dispatch
+      hello ()             ; action-name argspec
+        displayln "hello"  ; body
+      hi ()
         displayln "hi"
-      {name eq? 'disp}
-        displayln (car args)
-      else
-        #f
-
-;; We can easily generate something like this:
-define f2
-  λ (#:m [name '()] . args)
-    define hello
-      λ ()
-        displayln "hello"
-    define hi
-      λ ()
-        displayln "hi"
-    define disp
-      λ (s)
+      disp (s)
         displayln s
-    cond
-      {name eq? 'hello}
-        (apply hello args)
-      {name eq? 'hi}
-        (apply hi args)
-      {name eq? 'disp}
-        (apply disp args)
-      else
-        #f
-
-;; Automated macro version:
-define f3
-  λ/dispatch
+      else args            ; default action, here with varargs (see λ)
+        displayln args
+  ;
+  ; abbreviated form
+  define-dispatcher f4
     hello ()             ; action-name argspec
       displayln "hello"  ; body
     hi ()
@@ -98,43 +112,32 @@ define f3
       displayln s
     else args            ; default action, here with varargs (see λ)
       displayln args
-
-; abbreviated form
-define-dispatcher f4
-  hello ()             ; action-name argspec
-    displayln "hello"  ; body
-  hi ()
-    displayln "hi"
-  disp (s)
-    displayln s
-  else args            ; default action, here with varargs (see λ)
-    displayln args
-
-; testing without the else clause
-define-dispatcher f5
-  hello ()             ; action-name argspec
-    displayln "hello"  ; body
-  hi ()
-    displayln "hi"
-  disp (s)
-    displayln s
-
-(f1 #:m 'hello)
-(f1 #:m 'disp "foo")
-(f1 "baa" "goes" "the" "sheep")
-
-(f2 #:m 'hello)
-(f2 #:m 'disp "foo")
-(f2 "baa" "goes" "the" "sheep")
-
-(f3 #:m 'hello)
-(f3 #:m 'disp "foo")
-(f3 "baa" "goes" "the" "sheep")
-
-(f4 #:m 'hello)
-(f4 #:m 'disp "foo")
-(f4 "baa" "goes" "the" "sheep")
-
-(f5 #:m 'hello)
-(f5 #:m 'disp "foo")
-(f5 #:m '?)  ; ask what actions f5 has
+  ;
+  ; testing without the else clause
+  define-dispatcher f5
+    hello ()             ; action-name argspec
+      displayln "hello"  ; body
+    hi ()
+      displayln "hi"
+    disp (s)
+      displayln s
+  ;
+  (f1 #:m 'hello)
+  (f1 #:m 'disp "foo")
+  (f1 "baa" "goes" "the" "sheep")
+  ;
+  (f2 #:m 'hello)
+  (f2 #:m 'disp "foo")
+  (f2 "baa" "goes" "the" "sheep")
+  ;
+  (f3 #:m 'hello)
+  (f3 #:m 'disp "foo")
+  (f3 "baa" "goes" "the" "sheep")
+  ;
+  (f4 #:m 'hello)
+  (f4 #:m 'disp "foo")
+  (f4 "baa" "goes" "the" "sheep")
+  ;
+  (f5 #:m 'hello)
+  (f5 #:m 'disp "foo")
+  (f5 #:m '?)  ; ask what actions f5 has
