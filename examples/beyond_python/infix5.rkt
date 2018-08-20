66f2772c49314673e3c3edb8e828927e532c07c5
Juha Jeronen <juha.jeronen@jyu.fi>
2018-08-21 00:37:37 +0300

    rewrite in modern style

--- examples/beyond_python/infix5.rkt
+++ examples/beyond_python/infix5.rkt
@@ -37,21 +37,16 @@ provide nfx
 ;; TODO: improve messages for syntax errors; currently the "at:" and "in:" are the same
 ;; TODO: because #:fail-unless does not allow specifying them manually (unlike raise-syntax-error).
 ;;
-require (for-syntax syntax/parse)
-define-syntax (nfx stx)
-  syntax-parse stx
-    [(_ x0:expr) #'x0]
-    [(_ x0:expr op:id x:expr ...)
-     #:fail-unless odd-length?(#'(x0 op x ...))
-                   "last operand missing, expected x0 [op x] ..."
-     #:fail-unless op-arg-pairs?(#'(op x ...))
-                   "non-identifier in operator position, expected x0 [op x] ..."
-     ;infix-to-prefix-left-to-right(#'(x0 op x ...))]  ; choose either implementation here
-     infix-to-prefix-with-precedence(#'(x0 op x ...))]
+require syntax/parse/define
+require
+  for-syntax
+    syntax/parse
+    racket/match
+    only-in racket/list empty empty?
+    only-in racket/function curry
 
 ;; Each arg can be any s-expr, so we don't do any checking on that.
-;; We just check that, when converted a list, stx has identifiers
-;; in positions 0, 2, 4, ...
+;; We just check that stx has identifiers in even-numbered positions (starting from 0).
 ;;
 ;; Why identifiers? An s-expr such as (if (x < 0) (-) (+)) is a valid operator in Racket,
 ;; but the nfx variant with precedence cannot work with arbitrary s-expr operators,
@@ -64,24 +59,13 @@ define-syntax (nfx stx)
 ;; used in the expression, and raises a syntax error if the result did not reduce due to
 ;; unknown operators being present.
 ;;
-require (for-syntax (only-in racket/list empty? empty))
-begin-for-syntax
-  define op-arg-pairs?(stx)
-    ;; we don't need any lexical context here, so syntax->datum on the whole input is fine
-    define L syntax->datum(stx)
-    define op cond( [{length(L) > 0} datum->syntax(stx car(L))]
-                    [else datum->syntax(stx empty)] )
-    define rest cond( [{length(L) > 2} datum->syntax(stx cddr(L))]
-                      [else datum->syntax(stx empty)] )
-    cond [empty?(L) #t]
-         [identifier?(op) op-arg-pairs?(rest)]
-         [else #f]
-
-begin-for-syntax
-  define odd-length?(stx)
-    ;; we don't need any lexical context here, so syntax->datum on the whole input is fine
-    odd? length(syntax->datum(stx))
-
+define-syntax-parser nfx
+  (_ x0:expr)
+    #'x0
+  (_ x0:expr (~and (~seq (~seq op:id x:expr) ...+)  ; The ~and pattern checks each term is a pair,
+                   (~seq id-expr-pair ...+)))       ; while also binding a name to the whole pair.
+    ;infix-to-prefix-left-to-right(#'(x0 id-expr-pair ...))  ; choose either implementation here
+    infix-to-prefix-with-precedence(#'(x0 id-expr-pair ...))
 
 ;; a) Left-to-right, no precedence.
 
@@ -89,23 +73,28 @@ begin-for-syntax
 ;;      {2 + 3 + 4 * 5} --> (* (+ 2 3 4) 5)
 begin-for-syntax
   define infix-to-prefix-left-to-right(stx)
-    define iter(prev-op a lst out)
-      cond [empty?(lst) reverse(cons(reverse(a) out))]  ; final commit
-           [else
-             define(op car(lst))
-             define(op-sym syntax->datum(op))  ; we compare just the symbol
-             define(b cadr(lst))
-             define(rest cddr(lst))
-             ;; at init, we use "a" to pass in car(L); everywhere else, "a" is a list
-             cond( [null?(prev-op) iter(op-sym list(b a op) rest out)]    ; init
-                   [eq?(op-sym prev-op) iter(op-sym cons(b a) rest out)]  ; buffer and continue
-                   [else iter(op-sym list(b reverse(a) op) rest out)])]   ; commit and reset
+    define loop(prev-op acc stxs)
+      match stxs
+        '()
+          reverse acc  ; final commit
+        (list-rest op x rest)
+          let ([op-sym (syntax->datum op)])  ; check the symbol only, ignoring what it's bound to
+            loop
+              op-sym
+              cond
+                (eq? prev-op 'none)     ; init
+                  list(x acc op)
+                (eq? op-sym prev-op)    ; same op-sym - buffer and continue
+                  cons x acc
+                else                    ; different op-sym - commit and reset
+                  list(x (reverse acc) op)
+              rest
     ;; syntax->list retains the lexical context (variable bindings etc.) at any inner levels
-    define L syntax->list(stx)
-    define result iter(null car(L) cdr(L) empty)
-    cond [{length(result) > 1}  ; the result should be a one-element list (containing nested lists)
-          raise-syntax-error('nfx format("failed with result ~a" result) stx)]
-    datum->syntax(stx car(result))
+    define result
+      match (syntax->list stx)
+        (cons x0 terms)
+          loop 'none x0 terms
+    datum->syntax stx result
 
 ;; b) With operations ordered by precedence.
 
@@ -131,47 +120,53 @@ begin-for-syntax
 
 begin-for-syntax
   define infix-to-prefix-with-precedence(stx)
-    define L syntax->list(stx)
     define result
-      for/fold ([lst L]) ([grp sym-groups])
-        reduce-given-ops(grp lst)
-    cond [{length(result) > 1}  ; the result should be a one-element list (containing nested lists)
-          raise-syntax-error('nfx format("failed with result ~a; maybe unknown op" result) stx)]
-    datum->syntax(stx car(result))
+      for/fold
+        ([acc (syntax->list stx)])
+        ([grp sym-groups])
+        reduce-given-ops grp acc
+    match result
+      (cons y '())  ; the result should be a one-element list (containing nested lists)
+        datum->syntax stx y
+      (cons y ys)
+        raise-syntax-error 'nfx format("failed with extra terms ~a" ys) stx
 
 ;; Reduce on given operators (symbols) only, pass the rest of L through as-is.
 ;; Here L is an infix expression represented as a list.
 begin-for-syntax
-  define reduce-given-ops(target-ops L)
-    define reverse-if-pair(x)
-      cond [pair?(x) reverse(x)] [else x]
-    define iter(prev-op a lst out)  ; here prev-op tracks only ops in target-ops
-      cond [empty?(lst)
-            reverse(cons(reverse-if-pair(a) out))]  ; final commit
-           [else
-             define(op car(lst))
-             define(op-sym syntax->datum(op))
-             define(b cadr(lst))
-             define(rest cddr(lst))
-             ;; when prev-op resets, we use "a" to pass in the first operand; otherwise, "a" is a list
-             cond( [null?(prev-op)
-                      cond( [member(op-sym target-ops) iter(op-sym list(b a op) rest out)]  ; start
-                            [else  ; not ours; copy out
-                               define(new-out foldl(cons out list(a op)))
-                               iter(null b rest new-out)])]  ; b becomes new a (single item!)
-                   ;; not null?(prev-op)
-                   [{member(op-sym target-ops) and eq?(op-sym prev-op)}
-                      iter(op-sym cons(b a) rest out)]  ; buffer and continue
-                   [else  ; reset and redo this step
-                      iter(null reverse(a) lst out)])]  ; "a" is done, finalize by reversing
-    ;; If L has only one subexpr, do nothing; this catches cases like {x * y expt z},
+  define reduce-given-ops(op-syms stx-list)
+    define reverse-if-pair(x) (cond [(pair? x) (reverse x)] [else x])
+    define loop(prev-op acc stxs out)  ; here prev-op tracks only ops in target-ops
+      match stxs
+        '()
+          reverse (cons (reverse-if-pair acc) out)  ; final commit
+        (list-rest op x rest)
+          let ([op-sym (syntax->datum op)])  ; check the symbol only, ignoring what it's bound to
+            cond
+              (eq? prev-op 'none)
+                cond
+                  (member op-sym op-syms)
+                    loop op-sym list(x acc op) rest out  ; init
+                  else  ; not ours; copy out
+                    ;; commit a and op to out; b becomes new a (single item!)
+                    loop 'none x rest (foldl cons out list(acc op))
+              ;; prev-op not 'none
+              {(eq? op-sym prev-op) and (member op-sym op-syms)}
+                loop op-sym (cons x acc) rest out  ; same op-sym (and ours) - buffer and continue
+              ;; prev-op not 'none; and op-sym either different from it, or not ours.
+              else  ; reset prev-op, commit acc, and redo this step
+                loop 'none (reverse acc) stxs out
+    ;; If expr has only one subexpr, do nothing; this catches cases like {x * y expt z},
     ;; where a <- (* x (expt y z)), and lst is empty, right at the start when we are called for (+ -).
     ;;
-    ;; iter() would reverse the only subexpr, because we construct everything else with the help of
-    ;; cons and reverse. Note that in all other cases, whenever iter() hits reverse(), "a" is a
+    ;; loop() would reverse the only subexpr, because we construct everything else with the help of
+    ;; cons and reverse. In all other cases, whenever loop() hits reverse(), "a" is a
     ;; *list of subexpressions*.
-    cond [{length(L) < 2} L]
-         [else iter(null car(L) cdr(L) empty)]
+    match stx-list
+      (cons x0 '())
+        stx-list
+      (cons x0 terms)
+        loop 'none x0 terms empty
 
 module+ main
   ;(nfx 2 * 3 * 5)  ; sweet-exp handles this case by default, so we call nfx manually to test it
