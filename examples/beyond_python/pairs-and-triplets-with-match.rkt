1445569967ffd430017dce86b3f41acd730c1fb6
Juha Jeronen <juha.jeronen@jyu.fi>
2018-09-04 14:13:00 +0300

    add ntuples

--- examples/beyond_python/pairs-and-triplets-with-match.rkt
+++ examples/beyond_python/pairs-and-triplets-with-match.rkt
@@ -22,5 +22,26 @@ define triplets(lst)
           pairs rest
         triplets rest
 
-pairs '(1 2 3 4)
-triplets '(1 2 3 4)
+define ntuples(n lst)
+   cond
+     {n = 1}
+       lst
+     else
+       match lst
+        '()
+          empty
+        [list-rest a rest]
+          append
+            map
+              cond
+                {n > 2}
+                  curry apply list a
+                else
+                  curry list a
+              ntuples {n - 1} rest
+            ntuples n rest
+
+define l '(1 2 3 4 5)
+pairs l
+triplets l
+ntuples 4 l
